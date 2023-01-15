ee9bc92e832da8fbac73fdb1d5e374decd8ff545
ac2dia <ac2dia@gmail.com>
2023-01-15 12:35:16 +0900

    Revert "feat: create echo framework sample"
    This reverts commit 3725ed3c1fd85979bf8c8e124bbe7fb216cb32e3.

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
+import "fmt"
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+	fmt.Println(hello())
+}
+
+func hello() string {
+	return "Hello Golang"
 }
