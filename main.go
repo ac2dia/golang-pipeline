3725ed3c1fd85979bf8c8e124bbe7fb216cb32e3
ac2dia <ac2dia@gmail.com>
2023-01-15 11:18:38 +0900

    feat: create echo framework sample

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println(hello())
-}
-
-func hello() string {
-	return "Hello Golang"
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
